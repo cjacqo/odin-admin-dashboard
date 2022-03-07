791b7bfeca2b6fad3dcc63a0a1d760abf729420b
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-07 02:43:08 -0700

    Initial setup. Files created and icon assets saved as objects

---
+++ js/icons.js
@@ -0,0 +1,57 @@
+const icons = {
+    nav: [
+        [
+            {
+                classNames: 'fa-solid fa-house-chimney',
+                text: 'Home'
+            },
+            {
+                classNames: 'fa-solid fa-address-card',
+                text: 'Profile'
+            },
+            {
+                classNames: 'fa-solid fa-envelope',
+                text: 'Messages'
+            },
+            {
+                classNames: 'fa-solid fa-clock',
+                text: 'History'
+            },
+            {
+                classNames: 'fa-solid fa-clipboard-list',
+                text: 'Tasks'
+            },
+            {
+                classNames: 'fa-solid fa-user-group',
+                text: 'Communities'
+            }
+        ],
+        [
+            {
+                classNames: 'fa-solid fa-gear',
+                text: 'Settings'
+            },
+            {
+                classNames: 'fa-solid fa-square-question',
+                text: 'Support'
+            },
+            {
+                classNames: 'fa-solid fa-shield-check',
+                text: 'Privacy'
+            }
+        ]
+    ],
+    card: [
+        {
+            classNames: 'fa-solid fa-star'
+        },
+        {
+            classNames: 'fa-solid fa-eye'
+        },
+        {
+            classNames: 'fa-solid fa-share-nodes'
+        }
+    ],
+    search: 'fa-solid fa-magnifying-glass',
+    bell: 'fa-solid fa-bell'
+}
